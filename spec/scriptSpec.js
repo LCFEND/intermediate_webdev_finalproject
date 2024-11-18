a69fee306d266624d38c719e0f580fd2c51cf724
lcfend <leecoombes@mail.de>
2024-11-18 08:55:40 -0500

    Final commit

---
+++ spec/scriptSpec.js
@@ -0,0 +1,13 @@
+const { calculateSimpleInterest, calculateTotalPayableAmount } = require('../src/script');
+
+describe("Interest Rate Calculator Tests", () => {
+  it("Calculate Simple Interest", () => {
+      const actual = calculateSimpleInterest(1000, 5, 1);
+      expect(actual).toBe(50);
+  });
+
+  it("Calculate Total Payable Amount", () => {
+      const actual = calculateTotalPayableAmount(1000, 50);
+      expect(actual).toBe(1050);
+  });
+});
